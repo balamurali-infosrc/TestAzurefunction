4ad65d5208dc66f17d3ffdb94e40434d84cb4ba0
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-05 12:45:44 +0530

    Azfun

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,7 +105,7 @@ resource "azurerm_app_service_plan" "func" {
 
 
 resource "azurerm_linux_function_app" "function" {
-  name                = var.function_name
+  name                = "func_linx"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_app_service_plan.func.id
