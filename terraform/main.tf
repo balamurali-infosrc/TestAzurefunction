840a4b89b595689d549c72ecc3b7f83ac03f07d3
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-05 09:55:05 +0530

    Azfun

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,8 +55,8 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  kind                = "linux"
-  reserved            = true
+  # kind                = "linux"
+  # reserved            = true
   sku_name = "B1"
 }
 
