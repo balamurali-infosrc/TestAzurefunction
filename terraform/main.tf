40f803a5e3d6d2d9c52e23917e6c6cfa37cb92a0
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-10-05 09:45:01 +0530

    Azfun

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_function_app" "function" {
   name                = var.function_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   functions_extension_version = "~4" # Azure Functions runtime version
